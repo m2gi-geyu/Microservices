d499a5691b644dbc1415c02825383c63bd437240
m2gi-elkasriy <youssef.el-kasri@etu.univ-grenoble-alpes.fr>
2023-01-13 13:19:44 +0100

    heroService done

---
+++ hero-service/src/main/java/me/escoffier/workshop/hero/HeroRessource.java
@@ -0,0 +1,26 @@
+package me.escoffier.workshop.hero;
+
+import org.jboss.logging.Logger;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Random;
+
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+public class HeroRessource {
+
+    private static final Logger LOGGER = Logger.getLogger(HeroRessource.class);
+
+    @GET
+    @Path("/heroes/random")
+    public Hero getRandomHero() {
+        Hero hero = Hero.findRandom();
+        LOGGER.debug("Found random hero " + hero);
+        return hero;
+    }
+
+
+}
